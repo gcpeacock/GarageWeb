a0ce8c4da13ca0f529c6320837acf0a54b7f4c0e
Gary Peacock <gcpeacock@gmail.com>
2022-07-18 19:48:03 -0400

    Added daily log rotation

--- backend/log.py
+++ backend/log.py
@@ -4,22 +4,35 @@ import RPi.GPIO as GPIO
 import time
 import threading
 from datetime import datetime
+from logging.handlers import TimedRotatingFileHandler
+
+LOG_FILE="/home/pi/GarageWeb/backend/garagewebapi/static/log.txt"
 
 #Set up logger to file and console
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)-6s %(levelname)-8s %(message)s',
-                    filename="/home/pi/GarageWeb/backend/garagewebapi/static/log.txt")
+logger = logging.getLogger("GARAGELOG")
+logger.setLevel(logging.DEBUG)
+
+# Set the logging to console
 console = logging.StreamHandler()
 console.setLevel(logging.DEBUG)
 # set a format which is simpler for console use
 formatter = logging.Formatter('%(asctime)-6s %(levelname)-8s %(message)s')
 # tell the handler to use this format
 console.setFormatter(formatter)
-# add the handler to the root logger
-logging.getLogger('').addHandler(console)
+
+rotation = TimedRotatingFileHandler(LOG_FILE,
+                                    when="d",
+                                    interval=1,
+                                    backupCount=7)
+formatter2 = logging.Formatter('%(asctime)-6s %(levelname)-8s %(message)s')
+rotation.setFormatter(formatter2)
 
 
-logging.info("Program Starting -- Hello!")
+# add the logging handlers
+logger.addHandler(console)
+logger.addHandler(rotation)
+
+logger.info("Program Starting -- Hello!")
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
@@ -34,7 +47,7 @@ class GarageDoor:
         self.DoorOpenTimerMessageSent = 1  #Turn off messages until timer is started
         GPIO.setup(self.pin1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.pin2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        logging.debug(self.DoorName + "'s Garage Door initialized on pins + " + str(self.pin1) + " and " + str(self.pin2))
+        logger.debug(self.DoorName + "'s Garage Door initialized on pins + " + str(self.pin1) + " and " + str(self.pin2))
 
 
     def run(self, _running):
@@ -43,40 +56,40 @@ class GarageDoor:
             if self.DoorOpenTimer == 1:  #Door Open Timer has Started
                 currentTimeDate = datetime.strptime(datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S'),'%Y-%m-%d %H:%M:%S')
                 if (currentTimeDate - self.TimeDoorOpened).seconds > 900 and self.DoorOpenTimerMessageSent == 0:
-                    logging.info(self.DoorName + "'s Garage Door has been Open for 15 minutes")
+                    logger.info(self.DoorName + "'s Garage Door has been Open for 15 minutes")
                     self.DoorOpenTimerMessageSent = 1
 
             if GPIO.input(self.pin1) == GPIO.HIGH and GPIO.input(self.pin2) == GPIO.HIGH:  #Door Status is Unknown
-                logging.info(self.DoorName + "'s Door is Opening/Closing")
+                logger.info(self.DoorName + "'s Door is Opening/Closing")
                 while GPIO.input(self.pin1) == GPIO.HIGH and GPIO.input(self.pin2) == GPIO.HIGH:
                     if _running() == False:
                         break
                     time.sleep(.5)
                 else:
                     if GPIO.input(self.pin1) == GPIO.LOW:  #Door is Closed
-                        logging.info(self.DoorName + "'s Door is Closed")
+                        logger.info(self.DoorName + "'s Door is Closed")
                         self.DoorOpenTimer = 0
 
                     if GPIO.input(self.pin2) == GPIO.LOW:  #Door is Open
-                        logging.info(self.DoorName + "'s Door is Open")
+                        logger.info(self.DoorName + "'s Door is Open")
                         #Start Door Open Timer
                         self.TimeDoorOpened = datetime.strptime(datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S'),'%Y-%m-%d %H:%M:%S')
                         self.DoorOpenTimer = 1
                         self.DoorOpenTimerMessageSent = 0
             else:
                 if GPIO.input(self.pin1) == GPIO.LOW and self.DoorOpenTimer == -1:  #Door is Closed
-                    logging.info(self.DoorName + "'s Door is Closed")
+                    logger.info(self.DoorName + "'s Door is Closed")
                     self.DoorOpenTimer = 0
 
                 if GPIO.input(self.pin2) == GPIO.LOW and self.DoorOpenTimer != 1:  #Door is Open
-                    logging.info(self.DoorName + "'s Door is Open")
+                    logger.info(self.DoorName + "'s Door is Open")
                     #Start Door Open Timer
                     self.TimeDoorOpened = datetime.strptime(datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S'),'%Y-%m-%d %H:%M:%S')
                     self.DoorOpenTimer = 1
                     self.DoorOpenTimerMessageSent = 0
 
         if _running() == False:
-            logging.debug(self.DoorName + " terminating")
+            logger.debug(self.DoorName + " terminating")
             
 
 # Create Garage Door instances
@@ -104,10 +117,10 @@ try:
 except KeyboardInterrupt:
     _running = False
     # Give the threads a chance to exit
-    logging.info("Shutting down the door monitors")
+    logger.info("Shutting down the door monitors")
     while threading.active_count() > 1:
-        logging.info(threading.active_count())
+        logger.info(threading.active_count())
         time.sleep(.5)
-    logging.info("Program Shutdown -- Goodbye!")
+    logger.info("Program Shutdown -- Goodbye!")
     GPIO.cleanup()
 
